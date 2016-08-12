6d16400b9adf488735e93178e7278f8b9edda172
bobovnii <ilya.bobovnikov@desy.de>
2016-08-12 12:12:20 +0200

    Update RunLimitsNEW.sh

--- NTupleMaker/test/SomeScripts/RunLimitsNEW.sh
+++ NTupleMaker/test/SomeScripts/RunLimitsNEW.sh
@@ -1,6 +1,8 @@
 
 #variables="DZeta_MTsum DZeta_MTtot met_DZeta met_MCTb met_MT met_MTsum met_MTtot MTsum_MCTb MTtot_MCTb"
-variables="MT2lester_MCTb MT2lester_met MT2lester_DZeta"
+#variables="MT2lester_MCTb MT2lester_met MT2lester_DZeta"
+variables="MT2lester_met"
+
 
 massp=$1
 lspp=$2
@@ -64,13 +66,12 @@ cardme=cards_me/${model}${mass}_LSP${masslsp}_${var}_11_me_${lumi}invpb.txt
 
 combineCards.py $cardmt $cardet  $cardme > $cardnew
 
-cardd=$cardnew
-
 fi
 
+cardd=$cardnew
 	cat bss > ljob_${model}_${mass}_LSP${masslsp}_${var}_${cut}_${channel}_${lumi}.sh
 
-	echo ./CreateTableLimit.sh ${model} ${mass} ${masslsp} ${var} ${cut} ${channel} ${lumi} >> ljob_${model}_${mass}_LSP${masslsp}_${var}_${cut}_${channel}_${lumi}.sh
+	echo ./CreateTableLimits.sh ${model} ${mass} ${masslsp} ${var} ${cut} ${channel} ${lumi} ${cardd} >> ljob_${model}_${mass}_LSP${masslsp}_${var}_${cut}_${channel}_${lumi}.sh
 	echo    mv higgsCombine${mass}_LSP${masslsp}_${var}.Asymptotic.mH${mass}.root outputTEST/${model}_${mass}_LSP${masslsp}_${var}_${cut}_${channel}_${lumi}invpb.root  >> ljob_${model}_${mass}_LSP${masslsp}_${var}_${cut}_${channel}_${lumi}.sh
 
 	qsub -N ${model}_${mass}_${masslsp}_${var} ljob_${model}_${mass}_LSP${masslsp}_${var}_${cut}_${channel}_${lumi}.sh
